8446b3e729a2be485226f70076ee01f860814822
gabizon103 <ehg54@cornell.edu>
2022-10-10 17:58:50 -0400

    Update valrdy

--- sim/valrdyqueue/valrdy.v
+++ sim/valrdyqueue/valrdy.v
@@ -1,13 +1,20 @@
 `ifndef PROJECT_VALRDYQUEUE_V
 `define PROJECT_VALRDYQUEUE_V
 
-module valrdy #(parameter bitwidth = 32) (snd_val, snd_rdy, snd_data_msg, rcv_val, rcv_rdy, rcv_data_msg);
+module valrdy #(parameter width = 32) (reset, snd_val, snd_rdy, snd_msg, rcv_val, rcv_rdy, rcv_msg);
     input logic snd_val;
     input logic [bitwidth-1:0] snd_data_msg;
+    input logic reset;
     input logic rcv_rdy;
     output logic snd_rdy;
     output logic [bitwidth-1:0] rcv_data_msg;
     output logic rcv_val;
 
-    ff flipflop1 
+    ff flipflop1 #(bitwidth = width) (
+	.EN(rcv_rdy & snd_val),
+	.CLK(CLK),
+	.IN(snd_msg),
+	.OUT(rcv_msg),
+	.RESET(reset)
+    );
     
