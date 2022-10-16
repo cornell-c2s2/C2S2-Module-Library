7bc340b2a23419b0baa8a3ee7795600a37e89358
UnsignedByte <edlam1889@gmail.com>
2022-10-15 19:05:04 -0700

    update library directory tree to subdirectories

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Iterates through all branches to pull verilog files into lib/
+
 # make sure we are on the main branch
 git switch main
 
 # clean library
-rm lib/sim/lib/*
+rm lib/* -r
 
 # reads all branches on this machine into a list 
 x=$(git branch -a --format="%(refname:short)" | egrep -v '^(origin/)?(main|HEAD)$') 
@@ -14,7 +16,7 @@ readarray -t x <<< "$x"
 for v in ${x[*]}
 do
 	# Find files of the form <folder>/sim/<folder>/<name>.v
-	f=$(git ls-tree -r $v --name-only | egrep ".+\.v$" | egrep -v "^lib/sim/lib/.+\.v$")
+	f=$(git ls-tree -r $v --name-only | egrep ".+\.v$" | egrep -v "^lib/.+\.v$")
 
 	echo -e "Reading files:
 \033[0;32m$f\033[0m
@@ -24,10 +26,12 @@ from branch \033[0;31m$v\033[0m
 	readarray -t f <<< "$f"
 
 	# Loop through every file in the file list
+	vstripped=$(egrep -o '[^/]+$' <<< $v)
+	mkdir lib/$vstripped
 	for file in ${f[*]}
 	do
 		# Calculate new path in the library directory
-		npath="lib/sim/lib/$(egrep -o "[^/]+\.v" <<< $file)"
+		npath="lib/$vstripped/$(egrep -o "[^/]+\.v" <<< $file)"
 		git show $v:$file > $npath
 	done
 done
