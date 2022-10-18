43959ada2705cc2512b58ee4d0a9349846b94b20
UnsignedByte <edlam1889@gmail.com>
2022-10-17 17:25:50 -0700

    random testing and bug fixes

--- multiplier/sim/multiplier/block_test/multiplier_test.py
+++ multiplier/sim/multiplier/block_test/multiplier_test.py
@@ -4,30 +4,50 @@ from pymtl3.passes.PassGroups import DefaultPassGroup
 from pymtl3.passes.backends.verilog import *
 from fxpmath import Fxp
 from multiplier import fpmulit
+from random import randint
 
-def eval_until_ready(model, a, b):
-	model.a @= int(a.bin(), 2)
-	model.b @= int(b.bin(), 2)
-	model.snd_val @= 1
-	model.sim_eval_combinational()
+# return a random fxp value
+def rand_fxp(s, n, d):
+	x = Fxp(0, s, n, d, overflow='wrap')
+	x.set_val(randint(0, 2**n-1), raw=True)
+	x.config.op_sizing = 'same'
+	return x
 
-	for _ in range(100):
-		if model.snd_rdy == 1:
-			break;
-		model.sim_tick()
-	
-	return model.c
-
-
-def test_with_dump():
-	n, d, s = 32, 16, 1 # 32 bit signed fixed point numbers with 16 bits allocated for decimal
-
+# Initialize a simulatable model
+def create_model(n, d, s, dump_vcd=None):
 	model = fpmulit(n, d, s)
 	model.elaborate()
 	model.apply(VerilogPlaceholderPass())
 	model = VerilogTranslationImportPass()( model )
-	model.apply(DefaultPassGroup(vcdwave='test_dump'))
+	if dump_vcd is not None:
+		model.apply(DefaultPassGroup(vcdwave=dump_vcd))
+	else:
+		model.apply(DefaultPassGroup())
 	model.sim_reset()
+
+	return model
+
+# run the multiplier until output
+def eval_until_ready(model, a, b, max_cycles = 1000):
+	model.a @= int(a.bin(), 2)
+	model.b @= int(b.bin(), 2)
+	model.snd_val @= 1
+	model.sim_eval_combinational()
+	
+	while int(model.snd_rdy) == 1:
+		model.sim_tick()
+
+	for _ in range(max_cycles): # Abort after too many cycles have passed
+		if int(model.rcv_val) == 1:
+			return model.c
+		model.sim_tick()
+	
+	return None
+
+def test_with_dump():
+	n, d, s = 32, 16, 1 # 32 bit signed fixed point numbers with 16 bits allocated for decimal
+	
+	model = create_model(n, d, s, dump_vcd='test_with_dump');
 
 	a = Fxp(3.7, s, n, d, overflow='wrap')
 	a.config.op_sizing = 'same'
@@ -37,4 +57,52 @@ def test_with_dump():
 	out = Fxp(0, s, n, d, overflow='wrap')
 	out.set_val(int(eval_until_ready(model, a, b)), raw=True)
 
-	assert c == out
+	print(c.bin(), out.bin())
+
+	assert c.bin() == out.bin()
+
+def test_edge():
+	cases = [
+		(3, 0, 1, 3, 3), # overflow check
+		(2, 0, 0, 2, 2), # unsigned overflow check
+		(1, 1, 0, 1, 1), # 1 bit numbers (0.5 * 0.5 = 0)
+		(3, 3, 0, 0.5, 0.5), # unsigned number with no non-decimal bits
+	]
+
+	for (n, d, s, a, b) in cases:
+		a = Fxp(a, s, n, d, overflow='wrap')
+		a.config.op_sizing = 'same'
+		b = Fxp(b, s, n, d, overflow='wrap')
+
+		model = create_model(n, d, s, dump_vcd='edge')
+
+		out = Fxp(0, s, n, d, overflow='wrap')
+		out.set_val(int(eval_until_ready(model, a, b)), raw=True)
+
+		c = a * b
+		print("%s * %s = %s, got %s" % (a.bin(), b.bin(), c.bin(), out.bin()))
+		assert c.bin() == out.bin()
+
+def test_random_individual():
+	mmn = (1, 32) # minimum and maximum number of bits to use
+	
+	for i in range(100):
+		n = randint(mmn[0], mmn[1])
+		s = randint(0, min(n-1,1)) # signed or not signed
+		d = randint(0, n-s) # decimal bits
+
+		a = rand_fxp(s, n, d)
+		b = rand_fxp(s, n, d)
+
+		model = create_model(n, d, s, dump_vcd='random_individual');
+
+		out = Fxp(0, s, n, d, overflow='wrap')
+		out.set_val(int(eval_until_ready(model, a, b)), raw=True)
+
+		c = a * b
+		print("%s * %s = %s, got %s" % (a.bin(frac_dot=True), b.bin(frac_dot=True), c.bin(frac_dot=True), out.bin(frac_dot=True)))
+		print("%s * %s = %s, got %s" % (a, b, c, out))
+		assert c.bin() == out.bin()
+
+
+
--- multiplier/sim/multiplier/fpmulit.v
+++ multiplier/sim/multiplier/fpmulit.v
@@ -1,44 +1,72 @@
 `ifndef FIXED_POINT_ITERATIVE_MULTIPLIER
 `define FIXED_POINT_ITERATIVE_MULTIPLIER
+`include "../../../lib/sim/nbitregister/RegisterV_Reset.v"
 
-interface state #( parameter n, parameter d ) ();
+interface stateI #( parameter n, parameter d ) ();
 	logic [n+d-1:0] acc; // this needs to be n+d bits because the decimal portion of the multiplication may overflow
 	logic [$clog2(n+1)-1:0] counter;
 
 	modport in ( input acc, input counter );
 	modport out ( output acc, output counter );
+	modport io ( inout acc, inout counter );
 endinterface
+
+module state #( parameter n, parameter d ) (clk, reset, w, dat, q);
+	input clk, reset, w;
+	stateI.in dat;
+	stateI.out q;
+	
+	RegisterV_Reset #(.N(n+d)) acc (
+		.clk(clk),
+		.reset(reset),
+		.w(w),
+		.d(dat.acc),
+		.q(q.acc)
+	);
+	RegisterV_Reset #(.N($clog2(n+1))) counter (
+		.clk(clk),
+		.reset(reset),
+		.w(w),
+		.d(dat.counter),
+		.q(q.counter)
+	);
+endmodule
 
 module fpmulit_inner # (
 	parameter n,
 	parameter d,
 	parameter sign
-) (clk, a, b, in, out, rdy);
-	input logic clk;
+) (a, b, in, out, rdy);
 	input logic [n-1:0] b;
 	input logic [n+d-1:0] a;
 	output logic rdy;
-	state.in in;
-	state.out out;
+	stateI.in in;
+	stateI.out out;
 
 	reg [n+d-1:0] tmp;
-	always @(posedge clk) begin
+	always @(*) begin
+		// $display("%b, %b\n", a, b);
 		if (in.counter < d) begin // we are multiplying to the right of the decimal point
-			tmp = in.acc >> 1;
-			if (b[in.counter])
+			tmp = in.acc;
+			if (b[in.counter]) begin
+				// $display("detected decimal bit\n");
 				tmp = tmp + a;
+			end
+			tmp = tmp >>> 1;
 		end else begin
 			tmp = 0;
-			if (b[in.counter])
+			if (b[in.counter]) begin
+				// $display("detected bit\n");
 				tmp = a << (in.counter - d);
-			if (sign & in.counter == n-1) begin// sign bit (if multiplier is signed)
-				tmp = -tmp;
 			end
+			if (sign & in.counter == n-1)// sign bit (if multiplier is signed)
+				tmp = -tmp;
 			tmp = tmp + in.acc;			
 		end
-		rdy <= in.counter == n-1;
-		out.acc <= tmp;
-		out.counter <= in.counter + 1;
+		// $display("counter: %d/%d, tmp: %b\n", in.counter, n, tmp);
+		rdy = in.counter == n-1;
+		out.acc = tmp;
+		out.counter = in.counter + 1;
 	end
 endmodule
 
@@ -49,7 +77,6 @@ module fpmulit
 	parameter sign = 1 // 1 if signed, 0 otherwise.
 ) (clk, snd_val, snd_rdy, rcv_val, rcv_rdy, a, b, c);
 	// performs the operation c = a*b
-	
 	input logic clk;
 	input logic snd_val, rcv_rdy;
 	input logic [n-1:0] a, b;
@@ -58,38 +85,43 @@ module fpmulit
 	reg [n-1:0] hb, hc;
 	reg [n+d-1:0] ha;
 	reg rdy;
-	state #(.n(n), .d(d)) currin, currout;
+	stateI #(.n(n), .d(d)) cinI, ctI;
 	
-	always @(posedge clk) begin
-		if (snd_val & snd_rdy) begin // we are ready to recieve data
-			currin.acc <= 0;
-			currin.counter <= 0;
-			ha <= {{d{a[n-1]}}, a};
-			hb <= b;
-			snd_rdy <= 0;
-		end else
-			currin.acc <= currout.acc;
-			currin.counter <= currout.counter;
+	initial begin
+		snd_rdy = 1;
 	end
-		
+
 	fpmulit_inner #(.n(n), .d(d), .sign(sign)) mult (
-		.clk(clk & ~rcv_val),
 		.a(ha),
 		.b(hb),
-		.in(currin),
-		.out(currout),
+		.in(cinI),
+		.out(ctI),
 		.rdy(rdy)
 	);
 
 	always @(posedge clk) begin
+		if (snd_val & snd_rdy) begin // we are ready to recieve data
+			ha <= {{d{(sign != 0) & a[n-1]}}, a};
+			hb <= b;
+			snd_rdy <= 0;
+			cinI.acc <= 0;
+			cinI.counter <= 0;
+		end
+
+		if (~snd_rdy) begin
+			cinI.acc <= ctI.acc;
+			cinI.counter <= ctI.counter;
+		end
+
 		if (rcv_rdy & rcv_val) begin // reciever is ready for input
 			snd_rdy <= 1;
 			rcv_val <= 0;
-		end else if (rdy & ~rcv_val) begin
-			rcv_val <= 1;
-			c <= currout.acc;
 		end
 
+		if (rdy & ~snd_rdy & ~rcv_val) begin
+			rcv_val <= 1;
+			c <= ctI.acc[n-1:0];
+		end
 	end
 endmodule
 `endif
