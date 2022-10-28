6ddd57a091d472d93cf68224fad0b7cf909c83f7
gabizon103 <ehg54@cornell.edu>
2022-10-28 02:29:17 -0400

    begin connecting top module

--- shiftreg.v
+++ shiftreg.v
@@ -1,12 +1,23 @@
-module shiftreg #(parameter bitwidth = 32) (clk, in, load_data, load_en, shift_en, out);
+`ifndef spi_master
+`define spi_master
+`endif
+
+module shiftreg #(parameter bitwidth = 32) (clk, reset, in, load_data, load_en, shift_en, out);
     input logic [bitwidth-1:0] load_data;
-    input logic clk, load_en, shift_en, in;
-    output logic [bitwidth-1:0] out;
+    input logic reset;
+    input logic load_en;
+    input logic shift_en;
+    input logic in;
+    input logic [bitwidth-1:0] load_data;
+    output logic out;
+
+    logic [bitwidth-1:0] regval;
  
     always @(posedge clk) begin
-        if (load_en) begin out <= load_data; end
-        else begin
-            if (shift_en) begin out <= {out[bitwidth-2:0], in}; end
-            else begin out <= out; end
+        if (reset) regval <= {bitwidth{1'b0}};
+        else if (load_en) regval <= load_data;
+        else if (~load_en & shift_en) regval <= {regval[bitwidth-2:0],in};
+        else regval <= regval;
     end
+    assign out = regval[bitwidth-1];
 endmodule
--- top.v
+++ top.v
@@ -1,1 +1,57 @@
+`ifndef spi_master
+`define spi_master
+`endif
 
+`include "spi_master.v"
+`include "shiftreg.v"
+
+module top (
+    // SPECIFY I/O HERE
+    input reset,
+    input clk,
+
+    input logic packet_size_ifc, // bit width?
+    input logic cs_addr_ifc,  // might exclude if we only use one cs
+    output logic send_val,
+    input logic send_rdy,
+    output logic recv_rdy,
+    input logic recv_val,
+    input recv_msg, // bit width?
+    output send_msg,
+    output cs0,
+    output sclk,
+    output mosi,
+    input miso
+);
+    
+    wire recv_rdy_out;
+    assign recv_rdy = recv_rdy_out;
+
+    wire sclk_negedge;
+    wire sclk_posedge;
+
+    spi_master_ctrl fsm (
+        //MAKE CONNECTIONS HERE
+        .recv_rdy(rcv_rdy_out),
+        .sclk_negedge(sclk_negedge)
+    );
+
+    shiftreg shregout (
+        .clk(clk),
+        .reset(reset),
+        .load_en(recv_val & recv_rdy_out),
+        .shift_en(sclk_negedge),
+        .in(1'b0),
+        .load_data(), //fill in
+        .out(mosi) 
+    );
+
+    shiftreg shregin (
+        .clk(clk),
+        .reset(reset),
+        .load_en(1'b0),
+        .shift_en(sclk_posedge),
+        .in(miso),
+        .load_data(1'b0),
+        .out(send_msg)
+    );
